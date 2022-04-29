8a3732dce0ce634158a765c7a557a8e40122606e
Dan Martin <dhmartin27@hotmail.com>
2022-04-28 22:47:30 -0400

    project finished

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,10 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const HDWalletProvider = require('truffle-hdwallet-provider');
+ //
+ const fs = require('fs');
+ const mnemonic = "abandon seat come vacant guess flush hello soul speak laugh nerve trash";//fs.readFileSync("../.secret").toString().trim();
 
 module.exports = {
   /**
@@ -72,6 +71,15 @@ module.exports = {
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+
+     rinkeby: {
+       provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/ed2f437cab28417bba185e06a0cfbe8c`),
+       network_id:4,       // Ropsten's id
+       gas: 5500000,        // Ropsten has a lower block limit than mainnet
+       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+     },
 
     // Useful for private networks
     // private: {
